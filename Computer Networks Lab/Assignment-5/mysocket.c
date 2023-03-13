d6c71058b67ec757ed647115f8874ca47728dbd9
Neeraj Boddeda <101348975+ssneeraj23@users.noreply.github.com>
2023-03-14 00:47:34 +0530

    not yet tested, but no compilation errors

--- Computer Networks Lab/Assignment-5/mysocket.c
+++ Computer Networks Lab/Assignment-5/mysocket.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -12,7 +13,10 @@
 #include <stdbool.h>
 
 #define SOCK_MyTCP 18
-
+int min(int a, int b)
+{
+     return a < b ? a : b;
+}
 typedef struct
 {
      int sock;
@@ -23,7 +27,7 @@ MyTCP main_socket;
 char **send_message;
 char **received_message;
 int send_table_len, recv_table_len;
-int send_table_index,recv_table_index;
+int send_table_index, recv_table_index;
 pthread_t Recvt, Sendt;
 pthread_cond_t cond_recv = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_send = PTHREAD_COND_INITIALIZER;
@@ -42,22 +46,123 @@ int my_send(MyTCP mysockfd, const void *buf, size_t len, int flags);
 int my_recv(MyTCP mysockfd, void *buf, size_t len, int flags);
 int my_close(MyTCP mysockfd);
 
-void *R(void *arg)
+int recv_cli(char *buf, int newsockfd)
 {
+     int recs = 100;
+     char msg_len_str[5];
+     bzero(msg_len_str, 5);
+     char t[1];
+     char recv_buf[recs + 1];
+     for (int i = 0; i < recs; ++i)
+          recv_buf[i] = '\0';
+     int c = 0;
+     int rs;
      while (1)
      {
-          pthread_lock(&mutex_recv);
+          rs=recv(newsockfd, t, 1, 0);
+          if(rs<=0)return rs;
+          strcat(msg_len_str, t);
+          if (t[0] == '\r')
+               break;
+     }
+     strcat(buf, msg_len_str);
+     int msg_len = atoi(msg_len_str);
+     int tbread = msg_len;
+     while (1)
+     {
+          rs = recv(newsockfd, recv_buf, min(recs, tbread), 0);
+          if(rs<=0)return rs;
+          recv_buf[rs] = '\0';
+          strcat(buf, recv_buf);
+          tbread -= rs;
+          c += rs;
+          if (tbread == 0)
+               break;
+     }
+     c += strlen(msg_len_str);
+     return c;
+}
+
+void *R(void *arg)
+{
+     int rs;
+     while (1)
+     {
+          pthread_mutex_lock(&mutex_recv);
           pthread_cond_wait(&cond_recv, &mutex_recv);
+          while (1)
+          {
+               if (recv_table_len != 10)
+               {
+                     rs=recv_cli( received_message[(recv_table_index+recv_table_len)%10], curr_socket.sock);
+                     if(rs<=0)break;
+                     ++recv_table_len;
+               }
+          }
      }
      return NULL;
 }
 
 void *S(void *arg)
 {
+     int tbsend = 0;
+     int msg_len, i;
+     char msg_len_str[4];
      while (1)
      {
-          pthread_lock(&mutex_send);
+          pthread_mutex_lock(&mutex_send);
           pthread_cond_wait(&cond_send, &mutex_send);
+          while (1)
+          {
+               if (send_table_len == 0)
+               {
+                    sleep(1);
+               }
+
+               if (tbsend == 0)
+               {
+                    bzero(msg_len_str, 4);
+                    i = 0;
+                    while (send_message[send_table_index][i] != '\r')
+                    {
+                         msg_len_str[i] = send_message[send_table_index][i];
+                         ++i;
+                    }
+                    msg_len = atoi(msg_len_str);
+                    tbsend = msg_len;
+                    if (1000 - i - 1 >= tbsend)
+                    {
+                         send(curr_socket.sock, send_message[send_table_index] + i + 1, tbsend, 0);
+                         tbsend = 0;
+                         send_table_index = (send_table_index + 1) % 10;
+                         --send_table_len;
+                    }
+                    else
+                    {
+                         send(curr_socket.sock, send_message[send_table_index] + i + 1, 1000 - i - 1, 0);
+                         tbsend = tbsend - 1000 + i + 1;
+                         send_table_index = (send_table_index + 1) % 10;
+                         --send_table_len;
+                    }
+               }
+               else
+               {
+                    if (tbsend <= 1000)
+                    {
+                         send(curr_socket.sock, send_message[send_table_index], tbsend, 0);
+                         tbsend = 0;
+                         send_table_index = (send_table_index + 1) % 10;
+                         --send_table_len;
+                    }
+                    else
+                    {
+                         send(curr_socket.sock, send_message[send_table_index], 1000, 0);
+                         tbsend -= 1000;
+                         send_table_index = (send_table_index + 1) % 10;
+                         --send_table_len;
+                    }
+               }
+          }
      }
      return NULL;
 }
@@ -92,11 +197,13 @@ MyTCP my_socket(int domain, int type, int protocol)
           for (int i = 0; i < 10; i++)
           {
                send_message[i] = (char *)malloc(1000 * sizeof(char));
+               bzero(send_message[i], 1000);
           }
           received_message = (char **)malloc(10 * sizeof(char *));
           for (int i = 0; i < 10; i++)
           {
-               received_message[i] = (char *)malloc(1000 * sizeof(char));
+               received_message[i] = (char *)malloc(5005 * sizeof(char));
+               bzero(received_message[i], 5005);
           }
           // create threads and initialize them
 
@@ -129,7 +236,8 @@ int my_listen(MyTCP mysockfd, int backlog)
 MyTCP my_accept(MyTCP mysockfd, struct sockaddr *cli_addr, socklen_t *clilen)
 {
      MyTCP newsocktcp;
-     newsockfd = accept(mysockfd.sock, (struct sockaddr *)&cli_addr, &clilen);
+     int newsockfd;
+     newsockfd = accept(mysockfd.sock, (struct sockaddr *)&cli_addr, clilen);
      if (newsockfd < 0)
      {
           newsocktcp.sock = -1;
@@ -191,27 +299,98 @@ int my_close(MyTCP mysockfd)
 
           close(mysockfd.sock);
      }
-     else {
-          //tharavatha rastha
+     else
+     {
+          // tharavatha rastha
      }
 }
 
 int my_send(MyTCP mysockfd, const void *buf, size_t len, int flags)
 {
-    char msg_len[4];
-    bzero(msg_len,4);
-    sprintf(msg,"%d",(int)len);
-    int row_req=(len+1+strlen(msg_len))/1000;
-    while(1)
-    {
-     if(row_req==0)break;
-     if(send_table_len<10)
+     char msg_len[4];
+     bzero(msg_len, 4);
+     sprintf(msg_len, "%d", (int)len);
+     int row_req = (len + 1 + strlen(msg_len)) / 1000;
+     int first = 0;
+     int ind = 0;
+     while (1)
      {
-          //add to table
-          --row_req;
-          continue;
+          if (row_req == 0)
+               break;
+          if (send_table_len < 10)
+          {
+               // add to table
+               if (first == 0)
+               {
+                    first = 1;
+                    strcpy(send_message[(send_table_index + send_table_len) % 10], msg_len);
+                    strcat(send_message[send_table_index], "\r");
+
+                    if (len > 1000 - strlen(msg_len) - 1)
+                    {
+                         strncat(send_message[(send_table_index + send_table_len) % 10], buf, 1000 - strlen(msg_len) - 1);
+                         ind = 1000 - strlen(msg_len) - 1;
+                    }
+                    else
+                    {
+                         strcat(send_message[(send_table_index + send_table_len) % 10], buf);
+                    }
+                    send_table_len++;
+               }
+               else
+               {
+                    if (len - ind > 1000)
+                    {
+                         strncpy(send_message[(send_table_index + send_table_len) % 10], buf + ind, 1000);
+                         ind = ind + 1000;
+                    }
+                    else
+                    {
+                         strcpy(send_message[(send_table_index + send_table_len) % 10], buf + ind);
+                    }
+                    send_table_index++;
+               }
+               --row_req;
+               continue;
+          }
+          return len;
+          pthread_mutex_lock(&mutex_dummy1);
+          pthread_cond_wait(&cond_send_full, &mutex_dummy1);
      }
-     pthread_lock(&mutex_dummy1);
-     pthread_cond_wait(&cond_send_full, &mutex_dummy1);
-    }
+}
+
+int my_recv(MyTCP mysockfd, void *buf, size_t len, int flags)
+{
+
+     char msg_len[4];
+     bzero(msg_len, 4);
+     int i = 0;
+     while (received_message[recv_table_index][i] != '\r')
+     {
+          msg_len[i] = received_message[recv_table_index % 10][i];
+          i++;
+     }
+     int num_of_bytes = atoi(msg_len);
+
+     while (1)
+     {
+          if (recv_table_len <= 0)
+          {
+               sleep(1);
+          }
+          else
+          {
+               if (len >= num_of_bytes)
+               {
+                    strcpy(buf, received_message[recv_table_index % 10] + i + 1);
+               }
+               else
+               {
+                    strncpy(buf, received_message[recv_table_index % 10] + i + 1, len);
+               }
+               bzero(received_message[recv_table_index % 10], 5005);
+               --recv_table_len;
+               recv_table_index++;
+          }
+     }
 }
